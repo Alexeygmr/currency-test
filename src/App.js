a33483fd7ed88b8ed2bd51d089511bbdcc29fe29
Alexeygmr <alexey@cravter.com>
2022-04-24 15:45:58 +0300

    fix warnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Tabs from "./components/Tabs/Tabs";
 import { useGetRatesQuery } from './redux/services/currencyRates'
-import { useParams,useNavigate } from "react-router-dom";
 const App = () => {
   const { data, error, isLoading } = useGetRatesQuery();
-  const navigate = useNavigate();
-  const params = useParams();
+
 
   return (
     <div className="App">
